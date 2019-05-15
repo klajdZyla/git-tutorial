8485a5d19f2e9de3b380c7e6f974ae5ed6d815ad
Lab ESPL Stud20 lab_espl_stud20 <lab_espl_stud20@tueircs-ldb10.int.rcs.ei.tum.de>
2019-05-15 12:13:12 +0200

    Coding Challenge complete

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "espl_lib.h"
 int main()
 {
-   printf("Hello ESPL\n");
+   unsigned int nr;
+   char ans;
+   do {
+      printf("Enter a number: ");
+      scanf("%d", &nr);
+      printf("%s\n", num_to_words(nr));
+      printf("Do you want to enter another number? (y/n)\n");
+      scanf(" %c", &ans);
+   } while(ans=='y');
    return 0;
 }
